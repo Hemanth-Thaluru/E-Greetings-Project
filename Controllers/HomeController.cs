37366147f2e2b2d152dc7d3a99a53d68793c7eeb
Hemanth <Hemanth@DESKTOP-IMI0QBR>
2021-08-13 13:12:47 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Dotnetcore_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnetcore_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Celebrations()
+        {
+            return View();
+        }
+        public IActionResult Festivals()
+        {
+            return View();
+        }
+
+        public IActionResult Invitation()
+        {
+            return View();
+        }
+
+
+        public IActionResult Cards()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Sigin()
+        {
+            return View();
+        }
+
+        public IActionResult Sigup()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
